67b30692338550eccb5a32b72ad2f16c3cf51f3a
AltinBajrami <altin.bajrami66@gmail.com>
2024-04-10 20:58:50 +0200

    Finished CRUD and UI for WeightClasses

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
   Events, About, HomeLayout, Landing, ResetPassword, Register, Login, Error, Users,
-  FightsFinish, CreateFightFinish, UpdateFightFinish, VerifyEmail, ForgotPassword, UpdateUser
+  FightsFinish, CreateFightFinish, UpdateFightFinish, VerifyEmail, ForgotPassword, UpdateUser,
+  WeightClasses, CreateWeightClasses, UpdateWeightClasses
 } from './pages'
 import { action as RegisterAction } from './pages/authPages/Register';
 import { action as ForgotPasswordAction } from './pages/authPages/ForgotPassword';
 import { loader as UsersLoader } from './pages/users/Users'
+import { loader as WeightClassLoader } from './pages/weightClasses/WeightClasses'
+import { loader as UpdateWeightClassLoader } from './pages/weightClasses/UpdateWeightClasses'
+import { action as UpdateWeightClassAction } from './pages/weightClasses/UpdateWeightClasses'
+import { action as CreateWeightClassAction } from './pages/weightClasses/CreateWeightClasses'
 
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5
@@ -40,7 +46,7 @@ const router = createBrowserRouter([
         path: 'fightFinish',
         element: <FightsFinish />
       },
-        {
+      {
         path: 'verify-email',
         element: <VerifyEmail />
       },
@@ -81,6 +87,22 @@ const router = createBrowserRouter([
       {
         path: 'fightFinish/update/:id',
         element: <UpdateFightFinish />
+      },
+      {
+        path: 'weightClasses',
+        element: <WeightClasses />,
+        loader: WeightClassLoader(queryClient)
+      },
+      {
+        path: 'weightClasses/create',
+        element: <CreateWeightClasses />,
+        action: CreateWeightClassAction(queryClient)
+      },
+      {
+        path: 'weightClasses/update/:id',
+        element: <UpdateWeightClasses />,
+        loader: UpdateWeightClassLoader(queryClient),
+        action: UpdateWeightClassAction(queryClient)
       },
     ]
   }
--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const connectDB = require('./db/connect');
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const fightFinishRouter = require('./routes/fightFinishRoutes');
-
+const weightClassesRouter = require('./routes/weightClassesRoutes');
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -46,7 +46,7 @@ app.use(express.static('./public'));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/fightFinish', fightFinishRouter);
-
+app.use('/api/v1/weightClasses', weightClassesRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
