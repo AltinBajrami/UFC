fe0dc4b6a6e91af3db85e1139fc7ef5116e54da0
AltinBajrami <86713759+AltinBajrami@users.noreply.github.com>
2024-04-11 17:07:00 +0200

    Getting from main (#12) (#14)
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * Create Finishs.js
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * getting user crud (#3)
    
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * Create UpdateFinish.jsx
    
    * created crud for fight finish
    
    * getting fightfinish (#4) (#5)
    
    * Create Finishs.js
    
    * Update README.md
    
    * getting user crud (#3)
    
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * Create UpdateFinish.jsx
    
    * created crud for fight finish
    
    ---------
    
    
    
    * Update App.jsx
    
    * Altin (#6)
    
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * Update index.js
    
    * Update index.js
    
    * Update App.jsx
    
    * Update App.jsx
    
    * Create Login,Register and verify email UI
    
    * Created Users,UpdateUser UI
    
    create Users,UpdateUsers,Reset password ,forgot password and delete user UI
    
    * removed react-modal package
    
    * Update App.jsx
    
    * Getting from main (#11)
    
    * getting fightFinish (#8)
    
    * Create Finishs.js
    
    * Update README.md
    
    * getting user crud (#3)
    
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * Create UpdateFinish.jsx
    
    * created crud for fight finish
    
    * getting fightfinish (#4) (#5)
    
    * Create Finishs.js
    
    * Update README.md
    
    * getting user crud (#3)
    
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * Create UpdateFinish.jsx
    
    * created crud for fight finish
    
    ---------
    
    
    
    * Update App.jsx
    
    * Altin (#6)
    
    * Added SubMenu Component
    
    Create SubMenu Component and Added Error Page
    
    * Update README.md
    
    * User Endpoints
    
    create user endpoints like getAllUsers,getUser,showUser,updateUser and updatePassword
    
    * Update index.js
    
    ---------
    
    
    
    * Update App.jsx
    
    ---------
    
    
    
    * Update Sidebar.jsx
    
    ---------
    
    Co-authored-by: dijanalatifi <118472976+dijanalatifi@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ const fightFinishRouter = require('./routes/fightFinishRoutes');
 const weightClassesRouter = require('./routes/weightClassesRoutes');
 
 
+
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -48,6 +49,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/fightFinish', fightFinishRouter);
 app.use('/api/v1/weightClasses', weightClassesRouter);
+
 
 
 app.use(notFoundMiddleware);
