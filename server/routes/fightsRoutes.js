fb764cfb61db09f1c870670b8fc9e5fd4928d176
getoarmorina3 <getoaarm1@gmail.com>
2024-04-15 23:59:53 +0200

    Fight and Ranked routes

---
+++ server/routes/fightsRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getAllFights,
+  getOneFight,
+  createFight,
+  updateFight,
+  deleteFight,
+} = require('../controllers/fightController');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+
+// Admin-only routes for fights
+router.get(
+  '/',
+  [authenticateUser, authorizePermissions('admin')],
+  getAllFights
+);
+router.get(
+  '/:id',
+  [authenticateUser, authorizePermissions('admin')],
+  getOneFight
+);
+router.post(
+  '/',
+  [authenticateUser, authorizePermissions('admin')],
+  createFight
+);
+router.patch(
+  '/:id',
+  [authenticateUser, authorizePermissions('admin')],
+  updateFight
+);
+router.delete(
+  '/:id',
+  [authenticateUser, authorizePermissions('admin')],
+  deleteFight
+);
+
+module.exports = router;
