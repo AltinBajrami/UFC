06eca1d2a618bafb61fff7f673f566ad29156722
getoarmorina3 <getoaarm1@gmail.com>
2024-04-19 00:01:54 +0200

    Update Fights.js

--- server/models/Fights.js
+++ server/models/Fights.js
@@ -1,44 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const FightSchema = new mongoose.Schema({
-  fightID: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-  },
-  miniEventID: {
-    type: mongoose.Types.ObjectId,
-    default: null,
-  },
   fighter1ID: {
     type: mongoose.Types.ObjectId,
-    ref: 'Fighter',
+    ref: "Fighter",
     required: true,
   },
   fighter2ID: {
     type: mongoose.Types.ObjectId,
-    ref: 'Fighter',
+    ref: "Fighter",
     required: true,
   },
   winnerID: {
     type: mongoose.Types.ObjectId,
-    ref: 'Fighter',
+    ref: "Fighter",
     required: true,
   },
   round: {
     type: Number,
     min: 0,
-    required: [true, 'Please provide the number of the round'],
+    max: 25,
+    required: [true, "Please provide the number of the round"],
   },
   time: {
-    type: String,
-    required: [true, 'Please provide the time of the fight'],
-    minlength: 3,
-    maxlength: 50,
+    type: Number,
+    required: [true, "Please provide the time of the fight in minutes max 25"],
+    minlength: 0,
+    maxlength: 25,
   },
   finishID: {
     type: mongoose.Types.ObjectId,
-    ref: 'finishs',
+    ref: "finishs",
     required: true,
+  },
+  miniEventID: {
+    type: mongoose.Types.ObjectId,
+    default: null,
   },
   refereeID: {
     type: mongoose.Types.ObjectId,
@@ -46,5 +43,5 @@ const FightSchema = new mongoose.Schema({
   },
 });
 
-const FightModel = mongoose.model('Fight', FightSchema);
+const FightModel = mongoose.model("Fight", FightSchema);
 module.exports = FightModel;
