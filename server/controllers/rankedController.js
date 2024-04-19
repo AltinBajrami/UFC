236fd1b416d2d119e0b43e9b843592efea8d3a65
getoarmorina3 <getoaarm1@gmail.com>
2024-04-19 13:06:28 +0200

    Update rankedController.js

--- server/controllers/rankedController.js
+++ server/controllers/rankedController.js
@@ -1,27 +1,56 @@
-const { StatusCodes } = require('http-status-codes');
-const { BadRequestError, NotFoundError } = require('../errors');
-const Ranked = require('../models/Ranked');
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+const Ranked = require("../models/Ranked");
 
 const getAllRanked = async (req, res) => {
-  const ranked = await Ranked.find({});
-  return res.status(StatusCodes.OK).json({ ranked });
+  try {
+    const ranked = await Ranked.find({})
+      .populate("weightClass", "className")
+      .populate("champion", "fighterName nickName")
+      .populate(
+        "rank1 rank2 rank3 rank4 rank5 rank6 rank7 rank8 rank9 rank10",
+        "fighterName nickName",
+      )
+      .exec();
+
+    return res.status(StatusCodes.OK).json({ ranked });
+  } catch (error) {
+    console.error("Error fetching ranked fighters:", error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error" });
+  }
 };
 
 const getOneRanked = async (req, res) => {
   const { id } = req.params;
 
-  // Find the ranked entry by ID
-  const ranked = await Ranked.findById(id);
-  if (!ranked) {
-    throw new NotFoundError('Ranked entry not found!');
-  }
+  try {
+    // Find the ranked entry by ID and populate the related fields
+    const ranked = await Ranked.findById(id)
+      .populate("weightClass", "className")
+      .populate("champion", "fighterName nickName")
+      .populate(
+        "rank1 rank2 rank3 rank4 rank5 rank6 rank7 rank8 rank9 rank10",
+        "fighterName nickName",
+      )
+      .exec();
 
-  return res.status(StatusCodes.OK).json({ ranked });
+    if (!ranked) {
+      throw new NotFoundError("Ranked entry not found!");
+    }
+
+    return res.status(StatusCodes.OK).json({ ranked });
+  } catch (error) {
+    console.error("Error fetching ranked fighter:", error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error" });
+  }
 };
 
 const createRanked = async (req, res) => {
   const {
-    rankedID,
     weightClass,
     champion,
     rank1,
@@ -37,15 +66,14 @@ const createRanked = async (req, res) => {
   } = req.body;
 
   // Check if all required fields are provided
-  if (!rankedID || !weightClass) {
+  if (!weightClass) {
     throw new BadRequestError(
-      'Please provide all necessary information to create a ranked entry'
+      "Please provide all necessary information to create a ranked entry",
     );
   }
 
   // Create the ranked entry
   const newRanked = await Ranked.create({
-    rankedID,
     weightClass,
     champion,
     rank1,
@@ -82,14 +110,14 @@ const updateRanked = async (req, res) => {
   // Check if all required fields are provided
   if (!weightClass) {
     throw new BadRequestError(
-      'Please provide all necessary information to update a ranked entry'
+      "Please provide all necessary information to update a ranked entry",
     );
   }
 
   // Find the ranked entry by ID
   let ranked = await Ranked.findById(id);
   if (!ranked) {
-    throw new NotFoundError('Ranked entry not found!');
+    throw new NotFoundError("Ranked entry not found!");
   }
 
   // Update the ranked entry
@@ -114,10 +142,10 @@ const deleteRanked = async (req, res) => {
   const { id } = req.params;
   const ranked = await Ranked.findById(id);
   if (!ranked) {
-    throw new NotFoundError('Ranked entry not found!');
+    throw new NotFoundError("Ranked entry not found!");
   }
   await ranked.deleteOne();
-  return res.status(StatusCodes.OK).json({ msg: 'Ranked entry deleted' });
+  return res.status(StatusCodes.OK).json({ msg: "Ranked entry deleted" });
 };
 
 module.exports = {
